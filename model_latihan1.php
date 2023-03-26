ad7b403a55237960decc1c1745013d08ee3d91d6
Jeana-Karen <karenaisya@gmail.com>
2023-03-26 17:22:53 +0700

    aaa

--- model_latihan1.php
+++ model_latihan1.php
@@ -9,6 +9,7 @@ class model_latihan1 extends CI_Model
         $this->nilai1 = $n1;
         $this->nilai2 = $n2;
         $this->hasil = $this->nilai1 + $this->nilai2;
-        return $this->hasil;
+        return $this->hasil; 
+        // hhhhhhhhhhh
     }
 }
